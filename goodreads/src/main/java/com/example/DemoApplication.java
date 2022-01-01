3d85bfc835f52ab3c5d4d99b915426e5bf254f98
Nitin Bisht <3498georgian@gmail.com>
2022-01-01 21:11:32 +0530

    Save a single record (Author) in the Astra DB

--- goodreads/src/main/java/com/example/DemoApplication.java
+++ goodreads/src/main/java/com/example/DemoApplication.java
@@ -1,17 +1,24 @@
-package com.example.demo;
+package com.example;
 
+import com.example.author.Author;
+import com.example.author.AuthorRepository;
 import com.example.connection.DataStaxAstraProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class DemoApplication {
+
+    @Autowired
+    AuthorRepository authorRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -25,5 +32,14 @@ public class DemoApplication {
     public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
         Path bundle = astraProperties.getSecureConnectBundle().toPath();
         return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+
+    @PostConstruct
+    public void start() {
+        Author author = new Author();
+        author.setId("Asfasasd");
+        author.setName("Nitin Bisht");
+        author.setPersonalName("nitin bisht");
+        authorRepository.save(author);
     }
 }
